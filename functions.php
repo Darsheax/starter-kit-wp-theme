190be154f5290c12c752136f7e6b05201c3e00d6
Darsheax <darsheax.program@gmail.com>
2021-07-19 10:11:30 +0200

    build(wordpress): ajout functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,6 @@
 <?php
+
+function themenpm_scripts(){
+    wp_enqueue_script('theme-npm-scripts', get_template_directory_uri() . '/prod/index.js');
+}
+add_action('wp_enqueue_scripts', 'themenpm_scripts');
